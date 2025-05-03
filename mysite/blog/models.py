78c17b9fba860d54480a41e44323ca61885ad6c4
kukold <vlados.18@bk.ru>
2025-05-03 19:15:46 +0700

    почта

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -39,4 +39,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("blog:post_detail", args=[self.slug, self.publish])
+        return reverse(
+            "blog:post_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
