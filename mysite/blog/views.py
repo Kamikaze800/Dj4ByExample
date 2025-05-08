eb78d5a040fed9998e5027dcc81fb615f855945c
oldkuk <vlados.18@bk.ru>
2025-05-08 20:24:00 +0700

    мда как-то так

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import CommentForm, EmailPostForm
 from django.core.mail import send_mail
+from django.views.decorators.http import require_POST
 
 
 def post_share(request, post_id):
@@ -59,5 +60,26 @@ def post_detail(request, year, month, day, slug):
         publish__month=month,
         publish__day=day,
     )
-    print(request.method)
-    return render(request, "blog/post/detail.html", {"post": post})
+    comments = Post.comments.filter(active=True)
+    form = CommentForm()
+    return render(
+        request,
+        "blog/post/detail.html",
+        {"post": post, "comments": comments, "form": form},
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status="PUBLISHED")
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(
+        request,
+        "blog/post/comment.html",
+        {"post": post, "comment": comment, "form": form},
+    )
